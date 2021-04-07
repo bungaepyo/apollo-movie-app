86af1239c70f8dbc0a5aef7f2933ee2d979c9983
bungaepyo <bungaepyo@gmail.com>
2021-04-06 21:50:27 -0400

    started using gql and react hooks

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-export default () => "Detail";
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+    let { id } = useParams();
+    id = parseInt(id)
+    const { loading, data } = useQuery(GET_MOVIE, {
+      variables: { id }
+    });
+    if (loading) {
+      return "loading";
+    }
+    if (data && data.movie) {
+      return data.movie.title;
+    }
+  };
